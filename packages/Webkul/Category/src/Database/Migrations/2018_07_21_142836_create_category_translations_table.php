77929d9778ad158b08bd2813648da4d1f5455f96
sagar <sagar@einsicht-technologies.com>
2025-07-29 17:21:58 +0530

    Initial commit

---
+++ packages/Webkul/Category/src/Database/Migrations/2018_07_21_142836_create_category_translations_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->text('name');
+            $table->string('slug', 50);
+            $table->string('url_path', 2048);
+            $table->text('description')->nullable();
+            $table->text('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->integer('locale_id')->nullable()->unsigned();
+            $table->string('locale', 50);
+
+
+            $table->unique(['category_id', 'slug', 'locale']);
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('locale_id')->references('id')->on('locales')->onDelete('cascade');
+
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_translations');
+    }
+};
