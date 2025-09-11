2fe923b24b5df8b65725bb01817d91daf184818d
sagar <sagar@einsicht-technologies.com>
2025-09-11 17:57:51 +0530

    review api images added

--- packages/Webkul/Product/src/Models/ProductReview.php
+++ packages/Webkul/Product/src/Models/ProductReview.php
@@ -15,6 +15,22 @@ class ProductReview extends Model implements ProductReviewContract
 {
     use HasFactory;
 
+    /**
+     * Always eager load these relationships.
+     * Ensures API/serialization includes review images with URLs.
+     *
+     * @var array
+     */
+    protected $with = ['images'];
+
+    /**
+     * The accessors to append to the model's array form.
+     * Provides a simple list of image URLs alongside relations.
+     *
+     * @var array
+     */
+    protected $appends = ['image_urls'];
+
     protected $fillable = [
         'comment',
         'title',
@@ -47,6 +63,16 @@ class ProductReview extends Model implements ProductReviewContract
     public function images(): HasMany
     {
         return $this->hasMany(ProductReviewAttachmentProxy::modelClass(), 'review_id');
+    }
+
+    /**
+     * Get only the image URLs for convenience in API responses.
+     */
+    public function getImageUrlsAttribute(): array
+    {
+        return $this->images
+            ? $this->images->pluck('url')->filter()->values()->toArray()
+            : [];
     }
 
     /**
